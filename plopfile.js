1a001b7ee6fe4b566d6082f910fdbfbe194620a1
Alex Ros <alexrosmmii.ar@gmail.com>
2025-04-15 18:29:32 +0200

    New page to test components

--- plopfile.js
+++ plopfile.js
@@ -1,6 +1,7 @@
 export default function (plop) {
+    // Generador de componentes
     plop.setGenerator("component", {
-        description: "Create a component",
+        description: "Create new component",
         prompts: [
             {
                 type: "input",
@@ -23,6 +24,25 @@ export default function (plop) {
                 type: "add",
                 path: "src/components/{{pascalCase name}}/index.ts",
                 templateFile: "templates/index.ts.hbs",
+            },
+        ],
+    });
+
+    // Generador de páginas
+    plop.setGenerator("page", {
+        description: "Create new page",
+        prompts: [
+            {
+                type: "input",
+                name: "name",
+                message: "What is this page's name?",
+            },
+        ],
+        actions: [
+            {
+                type: "add",
+                path: "src/pages/{{pascalCase name}}Page.tsx",
+                templateFile: "templates/Page.tsx.hbs",
             },
         ],
     });
