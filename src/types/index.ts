f994915d65c8bb03ac112446e7eca8ae722c0f29
Alex Ros <alexrosmmii.ar@gmail.com>
2025-04-20 21:03:09 +0200

    added previous types for battle trails

---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export * from "./user";
+export * from "./post";
+export * from "./comment";
+export * from "./route";
+
+//use this to import all types from this file
+//import { User, Post, Comment, Route } from "@/types";
+
