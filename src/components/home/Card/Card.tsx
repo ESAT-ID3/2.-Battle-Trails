420ada57424943724bfb54c46672ed61c1d2155b
Alex Ros <alexrosmmii.ar@gmail.com>
2025-04-26 13:00:00 +0200

    features in types system on card component using post.ts

---
+++ src/components/home/Card/Card.tsx
@@ -0,0 +1,38 @@
+import {Eye, Bookmark, Share2} from "lucide-react";
+import {Post} from "@/types";
+
+
+const Card = ({post}: { post: Post }) => {
+    const {title, description, images, likes, likedBy} = post;
+
+    return (
+        <div className="card bg-base-100 w-96 rounded-field border border-neutral/20">
+            <figure>
+                <img
+                    src={images.length > 0 ? images[0] : "/placeholder.jpg"}
+                    alt={title}
+                    className="object-cover h-48 w-full"
+                />
+            </figure>
+            <div className="card-body gap-2.5">
+                <h2 className="card-title">{title}</h2>
+                <p>{description}</p>
+                <div className="flex justify-between">
+                    <div className="flex gap-2.5">
+                        <div className="flex items-center gap-1">
+                            <Eye size={18} strokeWidth={1.7}/> {likedBy.length}
+                        </div>
+                        <div className="flex items-center gap-1">
+                            <Bookmark size={18} strokeWidth={1.7}/> {likes}
+                        </div>
+                    </div>
+                    <div>
+                        <Share2 size={18} strokeWidth={1.7}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
