e448cfdbf5a5625513298d4029fb521ede794fd5
alexr <alexrosmmii.ar@gmail.com>
2025-05-05 18:20:24 +0200

    fixed header on top

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import SearchBox from "@components/ui/SearchBox/SearchBox";
 import logo from "@assets/btlogo_full.svg";
 
@@ -10,13 +10,14 @@ const Header = () => {
     };
 
     return (
-        <div className="py-5 bg-primary shadow-sm flex justify-between items-center px-4">
+        <div className="fixed top-0 left-0 w-full z-50 py-5 bg-primary shadow-sm flex justify-between items-center px-4">
+
             <div className="flex items-center basis-1/4 cursor-pointer" onClick={() => navigate("/")}>
-                <img src={logo} alt="logo" className="" />
+                <img src={logo} alt="logo" className=""/>
             </div>
 
             <div className="flex justify-center basis-1/2">
-                <SearchBox />
+                <SearchBox/>
             </div>
 
             <div className="flex justify-end basis-1/4 gap-4">
