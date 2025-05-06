b6d4ece34aa08095cb4da765baf8477af197c38f
alexr <alexrosmmii.ar@gmail.com>
2025-05-06 19:25:53 +0200

    auth-service created

---
+++ src/services/auth-service.ts
@@ -0,0 +1,38 @@
+// src/services/authService.ts
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    GoogleAuthProvider,
+    signOut,
+    onAuthStateChanged,
+    User,
+} from "firebase/auth";
+import { auth } from "@config/firebaseConfig.ts";
+
+// Registro con email y password
+export const registerWithEmail = (email: string, password: string) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+};
+
+// Login con email y password
+export const loginWithEmail = (email: string, password: string) => {
+    return signInWithEmailAndPassword(auth, email, password);
+};
+
+// Login con Google
+const googleProvider = new GoogleAuthProvider();
+
+export const loginWithGoogle = () => {
+    return signInWithPopup(auth, googleProvider);
+};
+
+// Logout
+export const logout = () => {
+    return signOut(auth);
+};
+
+// Escuchar el cambio de usuario autenticado (para Zustand, etc.)
+export const onAuthChange = (callback: (user: User | null) => void) => {
+    return onAuthStateChanged(auth, callback);
+};
