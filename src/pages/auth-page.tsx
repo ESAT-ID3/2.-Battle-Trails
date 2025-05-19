4b1f2bae4dfaf60a55a22c62192ce5dc6153e08a
Alex Ros <alexrosmmii.ar@gmail.com>
2025-05-19 11:37:26 +0200

    feat(auth): update AuthPage layout and integrate banner image for improved UI

--- src/pages/auth-page.tsx
+++ src/pages/auth-page.tsx
@@ -4,46 +4,47 @@ import {useAuth} from "@context/auth-context.tsx";
 import AuthAlert from "@components/auth/auth-alert/auth-alert.tsx";
 import {useState} from "react";
 import clsx from "clsx";
+import banner from "@assets/authimgs/banner-cell-phone-map-1.webp"
 
 
 const AuthPage = () => {
-    /*const { image, isLoaded } = useBackgroundImage();
-    const { errorMessage,clearError } = useAuth(); // obtenemos el error*/
+    const { image, isLoaded } = useBackgroundImage();
+    const { errorMessage,clearError } = useAuth(); // obtenemos el error
 
   const [isOnLeft, setIsOnLeft] = useState(true);
 
 
   return (
-        /*<div
-            className="min-h-screen flex items-center justify-center bg-neutral"
 
-        >
-            {/!* Alerta de error en top-right *!/}
-            {errorMessage && (
-                <div className="absolute top-6 right-6 z-50">
-                    <AuthAlert message={errorMessage} onClose={clearError}/>
-                </div>
-            )}
 
-            <AuthForm />
-        </div>*/
+    <div className="relative w-screen h-screen bg-base overflow-hidden">
 
-    <div className="relative w-screen h-screen bg-red-500 overflow-hidden">
-      {/* Contenido base (div A con hijos 1A y 2A) */}
+      {/* Alerta de error en top-right */}
+      {errorMessage && (
+        <div className="absolute top-6 right-6 z-50">
+          <AuthAlert message={errorMessage} onClose={clearError}/>
+        </div>
+      )}
+      {/* Forms  login/register */}
       <div className="flex w-full h-full">
-        <div className="w-1/2 flex items-center justify-center bg-orange-300">1A</div>
-        <div className="w-1/2 flex items-center justify-center bg-pink-300">2A</div>
+        <div className="w-1/2 flex items-center justify-center"> <AuthForm /></div>
+        <div className="w-1/2 flex items-center justify-center"> <AuthForm /></div>
       </div>
 
-      {/* Panel deslizante encima (div B) */}
+      {/* banner centrado en su mitad */}
       <div
         className={clsx(
-          "absolute top-0 h-200 w-200 p-10 w-1/2 bg-blue-500 transition-all duration-500 z-10",
+          "absolute top-0 h-full w-1/2 flex items-center justify-center transition-all duration-500 z-10",
           isOnLeft ? "left-0" : "left-1/2"
         )}
       >
-        <div className="relative h-full flex items-center justify-center text-white font-bold text-xl">
-          B
+        <div className="w-[95%] h-[95%] rounded-2xl relative flex items-center justify-center text-white text-xl font-bold shadow-lg"
+             style={{
+               backgroundImage: `url(${banner})`,
+               backgroundSize: "cover",
+               backgroundPosition: "center",
+             }}>
+
 
           {/* Botón izquierda */}
           <button
@@ -63,6 +64,7 @@ const AuthPage = () => {
         </div>
       </div>
     </div>
+
 
 
   );
